1e53ca60fd1501f06a4a22709da53ff85385e51b
JoseMarca17 <josemarca1374@gmail.com>
2025-06-03 20:42:41 -0400

    Cambios jose

--- usuario_tabla.php
+++ usuario_tabla.php
@@ -1,8 +1,8 @@
 <?php
 include('conexion.php');
 //include("barra_lateral.php");
+include ("header.php");
 ?>
-<?php include 'header.html'; ?> 
 <html>
 <title> registros </title>
 <link rel="stylesheet" href="styles/styles_tablas.css"> 
@@ -20,19 +20,19 @@ include('conexion.php');
         $pagina = 1;
     }
  
-//if(isset($_POST['btnbuscar']))
-// {
-// $buscar = $_POST['txtbuscar'];
+if(isset($_POST['btnbuscar']))
+{
+$buscar = $_POST['txtbuscar'];
 
-// $sqlusu = mysqli_query($conn, "SELECT * FROM usuarios where correo = '".$buscar."'");
+$sqlusu = mysqli_query($conn, "SELECT * FROM usuarios where correo = '".$buscar."'");
 
-//}
-//else
-//{
+}
+else
+{
     $sqlusu = mysqli_query($conn, "SELECT * FROM usuarios ORDER BY nombre DESC LIMIT " . (($pagina - 1) * $filasmax)  . "," . $filasmax);
-// }
+}
     $resultadoMaximo = mysqli_query($conn, "SELECT count(*) as num_usuarios FROM usuarios");
-   // $maxusutabla = mysqli_fetch_assoc($resultadoMaximo)['num_usuarios'];
+   $maxusutabla = mysqli_fetch_assoc($resultadoMaximo)['num_usuarios'];
     ?>
 	<div class="ContenedorTabla" >
 	<form method="POST">
@@ -66,11 +66,11 @@ include('conexion.php');
 			echo "<td>".$mostrar['ubicacion']."</td>";
 			echo "<td>*****</td>";
             echo "<td>".$mostrar['password']."</td>";    
-        //     echo  "<td style='width:24%'>
-	//		 <a class='BotonesUsuarios' href=\"usuarios_ver.php?correo=$mostrar[correo]&pag=$pagina\">Ver</a> 
-		//	<a class='BotonesUsuarios' href=\"usuarios_modificar.php?correo=$mostrar[correo]&pag=$pagina\">Modificar</a> 
-	 //<a class='BotonesUsuarios' href=\"usuarios_eliminar.php?correo=$mostrar[correo]&pag=$pagina\" onClick=\"return confirm('¿Estás seguro de eliminar a $mostrar[nombre]?')\">Eliminar</a>
-//	</td>";  
+            echo  "<td style='width:24%'>
+			 <a class='BotonesUsuarios' href=\"usuario_ver.php?correo=$mostrar[correo]&pag=$pagina\">Ver</a> 
+			<a class='BotonesUsuarios' href=\"usuario_modificar.php?correo=$mostrar[correo]&pag=$pagina\">Modificar</a> 
+	 <a class='BotonesUsuarios' href=\"usuario_eliminar.php?correo=$mostrar[correo]&pag=$pagina\" onClick=\"return confirm('¿Estás seguro de eliminar a $mostrar[nombre]?')\">Eliminar</a>
+	</td>";  
 			
         }
  
@@ -78,7 +78,7 @@ include('conexion.php');
     </table>
 	<div style='text-align:right'>
 	<br>
-	<?php // echo "Total de usuarios: ".$maxusutabla;?>
+	<?php echo "Total de usuarios: ".$maxusutabla;?>
 	</div>
 	</div>
 <div style='text-align:right'>
@@ -86,46 +86,46 @@ include('conexion.php');
 </div>
 <div style="text-align:center">
 <?php
-//if (isset($_GET['pag'])) {
-//if ($_GET['pag'] > 1) {
+if (isset($_GET['pag'])) {
+if ($_GET['pag'] > 1) {
  ?>
-<a class="BotonesUsuarios" href="usuarios_tabla.php?pag=<?php //echo $_GET['pag'] - 1; ?>">Anterior</a>
+<a class="BotonesUsuarios" href="usuarios_tabla.php?pag=<?php echo $_GET['pag'] - 1; ?>">Anterior</a>
 <?php
-//} 
-//else 
-//{
+} 
+else 
+{
 ?>
 <a class="BotonesUsuarios" href="#" style="pointer-events: none">Anterior</a>
 <?php
-// }
+}
 ?>
  
  <?php
-//} 
-// else 
-// {
+} 
+else 
+{
 ?>
 <a class="BotonesUsuarios" href="#" style="pointer-events: none">Anterior</a>
 <?php
-// }
+}
  
-// if (isset($_GET['pag'])) {
-// if ((($pagina) * $filasmax) < $maxusutabla) {
+if (isset($_GET['pag'])) {
+if ((($pagina) * $filasmax) < $maxusutabla) {
 ?>
-<a class="BotonesUsuarios" href="usuarios_tabla.php?pag=<?php //  echo $_GET['pag'] + 1; ?>">Siguiente</a>
+<a class="BotonesUsuarios" href="usuarios_tabla.php?pag=<?php echo $_GET['pag'] + 1; ?>">Siguiente</a>
 <?php
-// } else {
+} else {
 ?>
 <a class="BotonesUsuarios" href="#" style="pointer-events: none">Siguiente</a>
 <?php
-// }
+}
 ?>
 <?php
-// } else {
+} else {
 ?>
 <a class="BotonesUsuarios" href="usuarios_tabla.php?pag=2">Siguiente</a>
 <?php
- // }
+ }
 ?>
 </div>
 </div>
